228115ea96439c821502a7893a96572bcd105494
ReusCap <nick921@naver.com>
2026-04-02 15:44:52 +0900

    suite and setup and teardown

--- ID/test_id.py
+++ ID/test_id.py
@@ -8,8 +8,10 @@ class TestIDValidator(unittest.TestCase):
         
         # 성공 케이스 (영어 + 숫자 8자 이상)
         self.assertEqual(val.is_valid("python123"), True)
+        
         # 실패 케이스 (8자 미만)
         self.assertEqual(val.is_valid("py12"), False)
+        
         # 실패 케이스 (숫자 없음)
         self.assertEqual(val.is_valid("purealphabet"), False)
 
---
+++ ID/test_id_set_tear.py
@@ -0,0 +1,28 @@
+# test_id.py
+import unittest
+from id import IDValidator
+
+class TestIDValidator(unittest.TestCase):
+
+    def setUp(self):
+        print(">> setUp 실행됨")
+        # 모든 테스트 전에 새로운 IDValidator 객체 생성
+        self.validator = IDValidator()
+
+    def tearDown(self):
+        print("<< tearDown 실행됨")
+
+    def test_valid_id(self):
+        # 성공 케이스
+        self.assertEqual(self.validator.is_valid("python123"), True)
+
+    def test_short_id(self):
+        # 실패 케이스 (8자 미만)
+        self.assertEqual(self.validator.is_valid("py12"), False)
+
+    def test_only_alpha_id(self):
+        # 실패 케이스 (숫자 없음)
+        self.assertEqual(self.validator.is_valid("purealphabet"), False)
+
+if __name__ == '__main__':
+    unittest.main()
